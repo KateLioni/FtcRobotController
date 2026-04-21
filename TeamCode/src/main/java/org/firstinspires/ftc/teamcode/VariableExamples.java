5758712bbbb6bbcf2cce8bf9ed7c305eb82ce544
KateLioni <lionikate@gmail.com>
2026-04-21 14:03:40 +0200

    Create VariableExamples.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VariableExamples.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class VariableExamples {
+    //data types
+
+public class VariableExamples extends OpMode{
+    @Override
+    public void int ()
+
+    int number = 32000;
+    boolean active = true;
+    String name = "Katerina";
+    double time = 14.01;
+
+    telemetry.addData( "Number of team", number, " name is ", name)
+
+    }
+}
+
