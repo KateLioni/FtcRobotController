7e749aae49affd88c0ee4909b1bd9aa5b26650ca
KateLioni <lionikate@gmail.com>
2026-04-21 13:47:35 +0200

    Create HelloWorld.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp
+public class HelloWorld extends OpMode {
+    @Override
+    public void init() {
+        telemetry.addData("Hello", "World")
+    }
+    @Override
+    public void loop () {
+
+    }
+}
+
+@Autonomous
+public class HelloWorld extends OpMode {
+    @Override
+    public void init() {
+        telemetry.addData("Hello", "World")
+    }
+    @Override
+    public void loop () {
+
+    }
+}
